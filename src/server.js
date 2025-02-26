cd07b3c6c38e0818d61a05b73903dfc39a1deab8
NguyenDucVinh-18 <vnguyenduc187@gmail.com>
2025-02-26 20:48:07 +0700

    mySql

--- src/server.js
+++ src/server.js
@@ -4,12 +4,25 @@ require('dotenv').config()
 const app = express()
 const port = process.env.PORT
 const hostname = process.env.HOST_NAME
+const connection = require('./config/database')
 
 const configViewEngine = require('./config/viewEngine')
 configViewEngine(app)
 
 const webRoutes = require('./routes/web')
 app.use('/', webRoutes)
+
+
+
+connection.query(
+  'select * from users',
+  function(err, results, fields) {
+    console.log(results);
+  }
+)
+
+
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
