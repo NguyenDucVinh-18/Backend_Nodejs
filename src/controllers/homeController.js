3f14b6e8a59d3a4431eef7f1c9cd74d0014276ab
NguyenDucVinh-18 <vnguyenduc187@gmail.com>
2025-03-02 11:09:28 +0700

    listUser

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,30 +1,45 @@
 const connection = require("../config/database");
+const {getAllUsers} = require("../services/CRUDService");
 
-const getHomePage = (req, res) => {
-  return res.render("home.ejs");
+const getHomePage = async (req, res) => {
+  let result = await getAllUsers();
+  return res.render("home.ejs",{listUsers:result});
 };
 
 const getEjsPage = (req, res) => {
   res.render("sample.ejs");
 };
 
-const postCreateUser = (req, res) => {
+const postCreateUser = async (req, res) => {
   let name = req.body.name;
   let email = req.body.email;
   let city = req.body.city;
 
-  console.log(name, email, city);
-  connection.query(
-    `insert into users(email,name,city) values(? , ? , ?)`,
-    [email, name, city],
-    function (err, result) {
-      console.log(result);
-      res.send("User created successfully");
-    }
-  );
+  // console.log(name, email, city);
+  // connection.query(
+  //   `insert into users(email,name,city) values(? , ? , ?)`,
+  //   [email, name, city],
+  //   function (err, result) {
+  //     console.log(result);
+  //     res.send("User created successfully");
+  //   }
+  // );
+
+  let [results, fields] = await connection.query(
+    `insert into users(email,name,city) values(? , ? , ?)`, [email, name, city],
+  ) 
+  res.send("User created successfully");
+
+
+
 };
+
+const getCreateUserPage = (req, res) => {
+  res.render("createUser.ejs");
+}
 module.exports = {
   getHomePage,
   getEjsPage,
   postCreateUser,
+  getCreateUserPage,
 };
--- src/routes/web.js
+++ src/routes/web.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const {getHomePage, getEjsPage, postCreateUser} = require('../controllers/homeController');
+const {getHomePage, getEjsPage, postCreateUser,getCreateUserPage} = require('../controllers/homeController');
 
 router.get("/", getHomePage)
 router.get("/ejs",getEjsPage)
 router.post("/create-user",postCreateUser)
+router.get("/createUserPage",getCreateUserPage)
 
 
 module.exports = router;
